169ce25573e834561af35f15da9a1c2826c214e6
jedcastroag <jedcastroag@unal.edu.co>
2018-10-07 19:41:06 -0500

    terminado

--- lab3/inversiones_brute_force.c
+++ lab3/inversiones_brute_force.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
 #define N 1e+5
 
-long long inversions(int *A, int n)
+long long inversiones_brute_force(int *A, int n)
 {
     long long inv = 0;
     for (size_t i = 0; i < n-1; i++) {
-        for (size_t j = 0; j < n; j++) {
+        for (size_t j = i+1; j < n; j++) {
             if (A[i] > A[j]) {
                 inv++;
             }
@@ -19,6 +20,8 @@ long long inversions(int *A, int n)
 int main(int argc, char const *argv[]) {
     FILE *f;
     int *arr,n;
+    time_t s,t;
+    long long ans;
 
     f = fopen("input.txt", "r");
     if (f == NULL) {
@@ -26,9 +29,16 @@ int main(int argc, char const *argv[]) {
     }
     fscanf(f," %i",&n);
     arr = (int*) malloc(n*sizeof(int));
-    for (size_t i = 0; i < n; i++) {
+    for (size_t i = 0; i < N; i++) {
         fscanf(f," %i",&arr[i]);
     }
-    printf("%lli\n", inversions(arr,n));
+    fclose(f);
+    time(&s);
+    ans = inversiones_brute_force(arr,N);
+    time(&t);
+    free(arr);
+    t = t - s;
+    printf("%lli\n",ans);
+    printf("%1.3f\n", (float)t);
     return 0;
 }
--- lab3/inversiones_mergesort.c
+++ lab3/inversiones_mergesort.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
 #include <time.h>
-#include <math.h>
 #include <stdlib.h>
+#define N 1e+5
+
 long long merge(int *A, int s, int m, int e)
 {
-    int p = s,q = m+1,aux[e-s+1],i,k=0;
+    int p = s,q = m+1,*aux,i,k=0;
+    aux = (int*) calloc(e-s+1,sizeof(int));
     long long inv = 0;
     for(i = s; i <= e; ++i)
     {
@@ -20,19 +22,21 @@ long long merge(int *A, int s, int m, int e)
         }else
         {
             aux[k++] = A[q++];
-            inv += m-p+1;
+            inv += 1LL*(m-p+1);
         }
     }
     for(i = 0; i < k; ++i)
     {
         A[s+i]= aux[i];
     }
+    free(aux);
     return inv;
 }
 
-long merge_sort(int *A, int s, int e)
+
+long long merge_sort(int *A, int s, int e)
 {
-    long inv = 0;
+    long long inv = 0;
     if(s<e)
     {
         int mid = (s+e)/2;
@@ -42,32 +46,40 @@ long merge_sort(int *A, int s, int e)
     }
     return inv;
 }
+
+
 int main() {
     FILE *f;
     f = fopen("input.txt", "r");
     if(f == NULL){perror("error al abrir archivo..."); exit(-1);}
-    int n,r,i;
+    int n,r,i,j;
     time_t s,t;
+    int *A,*aux;
 
-    fscanf(f," %d",&n);
-    int A[n];
-    for(int i = 0; i < n; ++i)
+    fscanf(f," %i",&n);
+    n = N;
+    aux = (int*) calloc(n, sizeof(int));
+    A = (int*) calloc(n,sizeof(int));
+    for(i = 0; i < n; ++i)
     {
         fscanf(f," %d",&A[i]);
     }
     fclose(f);
-    r = 1000;
 
+    r = 100;
     time_t times[r];
     for (i = 0; i < r; ++i)
     {
+        for(j=0;j<n;++j)aux[j]=A[j];
         time(&s);
-        merge_sort(A, 0, n-1);
+         merge_sort(aux, 0, n-1);
+
         time(&t);
         t = t-s;
         times[i] = t;
     }
-
+    free(aux);
+    free(A);
     for (i = 1; i < r; ++i)
     {
         times[0] += times[i];
